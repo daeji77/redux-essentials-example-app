ec69fb312b053ee6ec37679ee4c84bb8aa1cce27
Cheol Park <daeji77@gmail.com>
2022-12-20 19:56:13 -0800

    Add AddPostForm

---
+++ src/features/posts/AddPostForm.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { postAdded } from './postsSlice';
+import { nanoid } from '@reduxjs/toolkit';
+
+export const AddPostForm = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const dispatch = useDispatch();
+
+  const onTitleChanged = e => setTitle(e.target.value)
+  const onContentChanged = e => setContent(e.target.value)
+  const onSavePostClicked = () => {
+    dispatch(postAdded({id: nanoid(), title, content}));
+
+    setTitle('');
+    setContent('');
+  }
+
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+        />
+        <label htmlFor="postContent">Post content:</label>
+        <textarea
+          type="text"
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={onContentChanged}
+        />
+        <button type="button" onClick={onSavePostClicked}>Save Post</button>
+      </form>
+    </section>
+  )
+}
