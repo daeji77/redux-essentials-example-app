c32d1d7311bfda70a60d81a7636b16f37b37645c
Cheol Park <daeji77@gmail.com>
2022-12-23 21:17:10 -0800

    Sort by date (descending order)

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,8 +1,11 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { sub } from 'date-fns'
 
 const initialState = [
-  { id: '1', title: 'First post', content: 'Hello', userId: '2' },
-  { id: '2', title: 'Second post', content: 'More text', userId: '1' },
+  { id: '1', date: sub(new Date(), { minutes: 10 }).toISOString(),
+    title: 'First post', content: 'Hello', userId: '2' },
+  { id: '2', date: sub(new Date(), { minutes: 5 }).toISOString(),
+    title: 'Second post', content: 'More text', userId: '1' },
 ]
 const postsSlice = createSlice({
   name: 'posts',
